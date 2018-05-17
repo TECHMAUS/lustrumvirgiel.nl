def52b19e600706d7efbd68e8299eff2d6322d8d
Maurits Misana <maurits@techmaus.nl>
2018-05-17 19:48:48 +0200

    New front-page

--- site/web/app/themes/lustrum-virgiel-xxiv/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/lustrum-virgiel-xxiv/resources/views/partials/entry-meta.blade.php
@@ -8,13 +8,5 @@
     <span class="icon is-small" style="background-image:url('@asset('images/news-entry/calendar.svg')')"></span>
     <time class="updated is-uppercase has-text-weight-bold" datetime="{{ get_post_time('c') }}">{{ get_the_date() }}</time>
   </div>
-    @if(! empty(get_the_category()))
-      <div class="meta-item">
-        <span class="icon is-small" style="background-image:url('@asset('images/news-entry/category.svg')')"></span>
-        <span>
-            {!! get_the_category_list(',&#32;')  !!}
-        </span>
-      </div>
-    @endif
 </div>
 
